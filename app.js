aec6c8bae5e216601db79f9d7d5674b17fc7492c
Daniel Bracho <danielbracho31898868@gmail.com>
2025-11-18 02:07:02 -0400

    Enable CORS preflight and add complete Swagger schemas

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
+app.options('*', cors());
 // Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
