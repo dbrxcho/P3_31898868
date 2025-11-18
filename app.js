3586852e4ee954def81ab4f6985ffb0b013ff368
Daniel Bracho <danielbracho31898868@gmail.com>
2025-11-18 01:54:01 -0400

    Fix Swagger servers and explicit CORS config

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
 // Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
